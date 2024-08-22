69ea41e9b3cd3c8ae0f7f13078eda3c8cd18bd78
Martin Fleischmann <martin@martinfleischmann.net>
2024-08-22 14:38:12 +0200

    fix imports

--- sgeop/__init__.py
+++ sgeop/__init__.py
@@ -1,3 +1,10 @@
+from . import simplify
 from .artifacts import get_artifacts
-from .nodes import consolidate_nodes, induce_nodes, remove_false_nodes, split
+from .nodes import (
+    consolidate_nodes,
+    fix_topology,
+    induce_nodes,
+    remove_false_nodes,
+    split,
+)
 from .simplify import simplify_loop, simplify_network
