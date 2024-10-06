c3436ca3b65c7286164e2ea3665327d1f1fcb2ed
James Gaboardi <jgaboardi@gmail.com>
2024-10-06 00:22:41 -0400

    gpd leq 1 testing polygonize

--- sgeop/tests/conftest.py
+++ sgeop/tests/conftest.py
@@ -3,6 +3,9 @@ import numpy
 import pandas
 import pytest
 import shapely
+from packaging.version import Version
+
+GPD_GE_100 = Version(geopandas.__version__) >= Version("1.0.0")
 
 line_collection = (
     list[shapely.LineString]
@@ -17,8 +20,15 @@ def polygonize(
     collection: line_collection, as_geom: bool = True
 ) -> shapely.Polygon | geopandas.GeoSeries:
     """Testing helper -- Create polygon from collection of lines."""
+
+    def _collection(c):
+        return shapely.polygonize(c)
+
     if isinstance(collection, pandas.Series | geopandas.GeoSeries):
-        _poly = geopandas.GeoSeries(collection).polygonize().buffer(0)
+        if GPD_GE_100:
+            _poly = geopandas.GeoSeries(collection).polygonize().buffer(0)
+        else:
+            _poly = geopandas.GeoSeries(_collection(list(collection)))
         if as_geom:
             return _poly.squeeze()
         else:
