2ccbace02f86bb81fcf2e79aace905e3f54d43ec
James Gaboardi <jgaboardi@gmail.com>
2024-10-12 14:30:01 -0400

    update geom testing helper

--- sgeop/tests/conftest.py
+++ sgeop/tests/conftest.py
@@ -13,6 +13,15 @@ line_collection = (
 )
 
 
+geometry_collection = (
+    list[shapely.GeometryCollection]
+    | tuple[shapely.GeometryCollection]
+    | numpy.ndarray
+    | pandas.Series
+    | geopandas.GeoSeries
+)
+
+
 def polygonize(
     collection: line_collection, as_geom: bool = True
 ) -> shapely.Polygon | geopandas.GeoSeries:
@@ -27,13 +36,28 @@ def polygonize(
         return shapely.polygonize(collection).buffer(0)
 
 
-def geom_test(collection1, collection2, relax=False):
+def is_geopandas(collection: geometry_collection) -> bool:
+    return isinstance(collection, geopandas.GeoSeries | geopandas.GeoDataFrame)
+
+
+def geom_test(
+    collection1: geometry_collection,
+    collection2: geometry_collection,
+    tolerance: float = 1e-1,
+) -> bool:
     """Testing helper -- geometry verification."""
-    geopandas.testing.assert_geoseries_equal(
-        geopandas.GeoSeries(collection1),
-        geopandas.GeoSeries(collection2),
-        check_less_precise=relax,
-    )
+
+    if not is_geopandas(collection1):
+        collection1 = geopandas.GeoSeries(collection1)
+
+    if not is_geopandas(collection2):
+        collection2 = geopandas.GeoSeries(collection2)
+
+    assert shapely.equals_exact(
+        collection1.geometry.normalize(),
+        collection2.geometry.normalize(),
+        tolerance=tolerance,
+    ).all()
 
 
 def pytest_configure(config):  # noqa: ARG001
