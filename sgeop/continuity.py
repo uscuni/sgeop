95b0aa3f84dbc66746dc0f3b1d6e8162808b5020
James Gaboardi <jgaboardi@gmail.com>
2024-10-20 14:50:05 -0400

    dedicated minimal tests for `continuity.py` (#50)
    * dedicated tests for continuity.py
    
    * full coverage continuity testing
    
    * fix typo in continuity() docstring
    
    * add CES description in get_stroke_info() docstring

--- sgeop/continuity.py
+++ sgeop/continuity.py
@@ -4,19 +4,27 @@ import momepy
 
 def continuity(
     roads: geopandas.GeoDataFrame, angle_threshold: float = 120
-) -> geopandas.GeoDataFrame:
+) -> tuple[geopandas.GeoDataFrame, momepy.COINS]:
     """Assign COINS-based information to roads.
 
     Parameters
     ----------
     roads :  geopandas.GeoDataFrame
         Road network.
+    angle_threshold : float = 120
+        See the ``angle_threshold`` keyword argument in ``momepy.COINS()``.
 
     Returns
     -------
-    geopandas.GeoDataFrame
-        The input ``roads`` with additional columns where the original
-        index may be reset (see ``dedup`` keyword argument).
+    roads : geopandas.GeoDataFrame
+        The input ``roads`` with additional columns describing COINS information.
+    coins : momepy.COINS
+        **This is not used in production.**
+
+    Notes
+    -----
+    The returned ``coins`` object is not used in production, but is
+    very helpful in testing & debugging. See gh:sgeop#49.
     """
     roads = roads.copy()
 
@@ -36,24 +44,35 @@ def continuity(
     return roads, coins
 
 
-def get_stroke_info(artifacts, roads):
-    """Generate information about strokes within the artifacts
+def get_stroke_info(
+    artifacts: geopandas.GeoSeries | geopandas.GeoDataFrame,
+    roads: geopandas.GeoSeries | geopandas.GeoDataFrame,
+) -> tuple[list[int]]:
+    """Generate information about strokes within ``artifacts`` and the
+    resulting lists can be assigned as columns to ``artifacts``. Classifies
+    the strokes within the CES typology.
 
-    Resulting lists can be assigned as columns to ``artifacts``.
+        * 'continuing' strokes - continues before and after artifact.
+        * 'ending' strokes - continues only at one end.
+        * 'single' strokes - does not continue.
 
     Parameters
     ----------
-    artifacts : GeoDataFrame | GeoSeries
-        Polygons representing the artifacts
-    roads : GeoDataFrame | GeoSeries
-        LineStrings representing the road network
+    artifacts : GeoSeries | GeoDataFrame
+        Polygons representing the artifacts.
+    roads : GeoSeries | GeoDataFrame
+        LineStrings representing the road network.
 
     Returns
     -------
-    stroke_count : list
-    C_count : list
-    E_count : list
-    S_count : list
+    strokes : list[int]
+        All strokes counts.
+    c_ : list[int]
+        Counts for 'continuing' strokes - continues before and after artifact.
+    e_ : list[int]
+        Counts for 'ending' strokes - continues only at one end.
+    s_ : list[int]
+        Counts for 'single' strokes - does not continue.
     """
     strokes = []
     c_ = []
@@ -63,31 +82,25 @@ def get_stroke_info(artifacts, roads):
         singles = 0
         ends = 0
         edges = roads.iloc[roads.sindex.query(geom, predicate="covers")]
-        if (  # roundabout special case
-            edges.coins_group.nunique() == 1
-            and edges.shape[0] == edges.coins_count.iloc[0]
-        ):
+        ecg = edges.coins_group
+        if ecg.nunique() == 1 and edges.shape[0] == edges.coins_count.iloc[0]:
+            # roundabout special case
             singles = 1
             mains = 0
         else:
             all_ends = edges[edges.coins_end]
-            mains = edges[
-                ~edges.coins_group.isin(all_ends.coins_group)
-            ].coins_group.nunique()
-
+            ae_cg = all_ends.coins_group
+            mains = edges[~ecg.isin(ae_cg)].coins_group.nunique()
             visited = []
-            for coins_count, group in zip(
-                all_ends.coins_count, all_ends.coins_group, strict=True
-            ):
-                if (group not in visited) and (
-                    coins_count == (edges.coins_group == group).sum()
-                ):
-                    singles += 1
-                    visited.append(group)
-                elif group not in visited:
-                    ends += 1
-                    # do not add to visited as they may be disjoint within the artifact
-        strokes.append(edges.coins_group.nunique())
+            for coins_count, group in zip(all_ends.coins_count, ae_cg, strict=True):
+                if group not in visited:
+                    if coins_count == (ecg == group).sum():
+                        singles += 1
+                        visited.append(group)
+                    else:
+                        # do not add to visited -- may be disjoint within the artifact
+                        ends += 1
+        strokes.append(ecg.nunique())
         c_.append(mains)
         e_.append(ends)
         s_.append(singles)
