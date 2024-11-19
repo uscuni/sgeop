c35d1c2fec2d3e122a53dd4989e859c20044632a
James Gaboardi <jgaboardi@gmail.com>
2024-11-19 11:09:33 -0500

    remove unneeded conditional -- nodes._status()

--- sgeop/nodes.py
+++ sgeop/nodes.py
@@ -78,12 +78,6 @@ def _status(x: pd.Series) -> str:
     """Determine the status of edge line(s)."""
     if len(x) == 1:
         return x.iloc[0]
-    if "new" in x.values:
-        # if "new" in x:
-        # This logic is here just to be safe. It will be hit if we create a new line
-        # and in a subsequent step extend it which is not what normally happens.
-        # All the new bits are caught likely by the first ``if``.
-        return "new"
     return "changed"
 
 
